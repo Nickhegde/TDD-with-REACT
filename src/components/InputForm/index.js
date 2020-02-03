9fb8b68a67086a52f80c2042cc9b8a86aaf1e766
Nikhil Hegde <nikhil.hegde@ymedialabs.com>
2020-02-03 11:18:04 +0530

    Add test cases to input form

--- src/components/InputForm/index.js
+++ src/components/InputForm/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import { useForm } from 'react-hook-form';
 import { STRINGS } from 'consts';
 
@@ -6,7 +7,12 @@ import './InputForm.scss';
 
 export default function InputForm() {
   const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = data => console.log(data);
+
+
+  const onSubmit = (data) => {
+    console.log(data);
+    const result = axios.post('http://172.16.2.66:8080/profile/upload-csv', formData, { 'Content-Type': 'multipart/form-data' });
+  };
   const [formData, setFormData] = useState('');
 
   const selectCSVFile = event => {
@@ -16,23 +22,23 @@ export default function InputForm() {
       event.target.files[0],
       event.target.files[0].name
     );
-    console.log("{ formDataVal }", formDataVal.getAll('file'))
+    // console.log("{ formDataVal }", formDataVal.getAll('file'))
     setFormData({ formDataVal });
   };
-  console.log("formData", formData);
+  // console.log("formData", formData);
   return (
     <form className='form-container' onSubmit={handleSubmit(onSubmit)}>
-      <input className='form-inputs' name="firstName" placeholder="First Name" ref={register({ required: true, maxlength: 20 })} />
-      <div className='error-msg'>{errors.firstName && 'First name is required'}</div>
-      <input className='form-inputs' name="lastName" placeholder="Last Name" ref={register({ required: true, maxlength: 20 })} />
-      <div className='error-msg'>{errors.lastName && 'Last name is required'}</div>
-      <select className='form-inputs' name="gender" ref={register}>
+      <input className='form-inputs first-name' name="firstName" placeholder="First Name" ref={register({ required: true, maxlength: 20 })} />
+      <div className='error-msg first-name-error'>{errors.firstName && 'First name is required'}</div>
+      <input className='form-inputs last-name' name="lastName" placeholder="Last Name" ref={register({ required: true, maxlength: 20 })} />
+      <div className='error-msg last-name-error'>{errors.lastName && 'Last name is required'}</div>
+      <select className='form-inputs gender' name="gender" ref={register}>
         <option value="male">Male</option>
         <option value="female">Female</option>
       </select>
-      <input className='form-inputs' name="age" type="number" placeholder="Age" ref={register({ required: true, min: 10 })} />
-      <input className='form-inputs' type="tel" placeholder="Mobile number" name="mobileNumber" ref={register({ required: true, pattern: { value: /^[0-9]{10}$/, message: 'Invalid phone number' } })} />
-      <div className='error-msg'>{errors.mobileNumber && (errors.mobileNumber.message || 'Mobile number is required')}</div>
+      <input className='form-inputs age' name="age" type="number" placeholder="Age" ref={register({ required: true, min: 10 })} />
+      <input className='form-inputs mobile-number' type="tel" placeholder="Mobile number" name="mobileNumber" ref={register({ required: true, pattern: { value: /^[0-9]{10}$/, message: 'Invalid phone number' } })} />
+      <div className='error-msg mobile-number-error'>{errors.mobileNumber && (errors.mobileNumber.message || 'Mobile number is required')}</div>
       <input className='form-upload-btn' type='file' accept=".csv" name="upload-csv-file" onChange={selectCSVFile} placeholder={STRINGS.CSV_UPLOAD} />
       <button className='form-submit-btn' type="submit">{STRINGS.SUBMIT}</button>
     </form>
--- src/components/_tests_/InputForm.test.js
+++ src/components/_tests_/InputForm.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { shallow } from 'enzyme';
+import { render, fireEvent } from '@testing-library/react';
+import { shallow, mount } from 'enzyme';
 import { InputForm } from 'components';
+import { spy } from 'sinon';
 
 describe('InputForm component', () => {
   const wrapper = shallow(<InputForm />);
@@ -12,13 +13,53 @@ describe('InputForm component', () => {
     expect(wrapper.find('option').length === 2);
     expect(wrapper.find('div').length === 3);
     expect(wrapper.find('button').length === 1);
-    // const Btn = wrapper.find('div.App-link');
-    // Btn.simulate('click');
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('InputForm inputs validation check for first name', () => {
-    const firstName = wrapper.find('form').childAt(0);
+  it('Input first name', () => {
+    const firstName = wrapper.find('input.first-name');
+    firstName.value = 'test first name';
+    expect(firstName.value === 'test first name');
+  });
+  it('Input last name', () => {
+    const lastName = wrapper.find('input.last-name');
+    lastName.value = 'test last name';
+    expect(lastName.value === 'test last name');
+  });
+
+  it('Input gender', () => {
+    const select = wrapper.find('select.gender');
+  });
+
+  it('Input age', () => {
+    const age = wrapper.find('input.age');
+    age.value = 10;
+    expect(age.value === 10);
+  });
+
+  it('Input mobile number', () => {
+    const mobile = wrapper.find('input.mobile-number');
+    mobile.value = 1000000000;
+    expect(mobile.value === 100000000);
+  });
+
+  it('Submit form by filling all required fields', () => {
+    const form = wrapper.find('form'),
+      firstName = wrapper.find('input.first-name'),
+      lastName = wrapper.find('input.last-name'),
+      age = wrapper.find('input.age'),
+      mobile = wrapper.find('input.mobile-number');
+    firstName.value = 'test first name';
+    lastName.value = 'test last name';
+    age.value = 10;
+    mobile.value = 1000000000;
+    form.simulate('submit');
+  })
+
+  it('Submit input form without entering any fileds adn check for error messages', async () => {
+    const form = wrapper.find('form');
+    form.simulate('submit');
+    console.log("div", form.find('div.first-name-error').html())
 
   });
 });
